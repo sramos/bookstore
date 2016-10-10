e4ce0e1c25b67c8f642be64eee02bc0d65e5afd5
sramos <sramos@sitiodistinto.net>
2016-10-10 23:57:51 +0200

    Changed ebooks upload dir

--- app/models/ebook.rb
+++ app/models/ebook.rb
@@ -1,7 +1,7 @@
 class Ebook < ActiveRecord::Base
   belongs_to :book
   has_attached_file :ebook, url: '/:class/:id/download/:style.:extension',
-                            path: ':rails_root/ebooks/:class/:attachment/:id_partition/:style/:filename'
+                            path: ':rails_root/private/:class/:attachment/:id_partition/:style/:filename'
 
   validates_attachment_content_type :ebook, content_type: ["application/vnd.amazon.ebook",
 							   "application/x-mobipocket-ebook",
