7ecf3bdd9f5ebd8e771aeb0159a5dd8a7b4d4c8a
sramos <sramos@sitiodistinto.net>
2016-10-11 00:56:10 +0200

    Images

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,6 @@ class Book < ActiveRecord::Base
   has_many :genres, through: :book_by_genres
   has_many :ebooks, dependent: :destroy
 
-  has_attached_file :cover, styles: { medium: "200x300", thumb: "100x150" }, default_url: "/images/:style/missing.png"
+  has_attached_file :cover, styles: { medium: "200x300", thumb: "100x150" }, default_url: "/images/no_cover_:style.gif"
   validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/
 end
