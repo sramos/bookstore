30d3921d0890ba55e350dc7e36bd120c21e0d5fe
sramos <sramos@sitiodistinto.net>
2016-11-09 11:12:17 +0100

    Restrict new books to logged users

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   # GET /books
